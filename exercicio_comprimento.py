31c2342b3dc71ede2ce696712aaf6049a03cc8e7
peter-loliveira <peter.loliveira@gmail.com>
2020-08-12 21:14:25 -0300

    Ajustes

--- exercicio_comprimento.py
+++ exercicio_comprimento.py
@@ -1,9 +1,11 @@
+tamanho_piso = .25
+
 sala_cump = float(input('SALA: Informa o COMPIMENTO:\n'))
 sala_larg = float(input('SALA: Informa o LARGURA:\n'))
 sala_area = sala_cump * sala_larg
 print('A SALA tem', format(sala_area, ".2f"), 'm2' )
 print('Serão gastos R$', format(sala_area * 30, ".2f") , 'para aplicar piso na SALA')
-print('Serão necessário', format(sala_area * 4, ".2f"), 'pisos para preencher a SALA')
+print('Serão necessário', format(sala_area /tamanho_piso, ".2f"), 'pisos para preencher a SALA')
 print('\n')
 
 quarto_cump = float(input('QUARTO: Informa o COMPIMENTO:\n'))
@@ -11,7 +13,7 @@ quarto_larg = float(input('QUARTO: Informa o LARGURA:\n'))
 quarto_area = quarto_cump * quarto_larg
 print('O QUARTO tem', format(quarto_area, ".2f"), 'm2' )
 print('Serão gastos R$', format( sala_area * 30, ".2f"), 'para aplicar piso no QUARTO')
-print('Serão necessário', format(sala_area * 4, ".2f"), 'pisos para preencher o QUARTO')
+print('Serão necessário', format(sala_area / tamanho_piso, ".2f"), 'pisos para preencher o QUARTO')
 print('\n')
 
 if sala_area != quarto_area:
@@ -29,7 +31,10 @@ if sala_area * 30 > 1000 and quarto_area * 30 >1000:
 else:    
     print('Em NENHUM dos comodos serão gastos mais de R$ 1.000,00')
 
-if sala_area * 4 >= 100 or quarto_area * 4 >= 100:
-    print('Em pelo menos UM dos comodos serão gastos MAIS que 100 pisos')
+if sala_area / tamanho_piso >= 100 or quarto_area / tamanho_piso >= 100:
+    print('Em pe3
+    lo menos UM dos comodos serão gastos MAIS que 100 pisos')
 else: 
-    print('Em NENHUM dos comodos serão gastos MAIS que 100 pisos'
+    print('Em NENHUM dos comodos serão gastos MAIS que 100 pisos')
+
+print('\n')

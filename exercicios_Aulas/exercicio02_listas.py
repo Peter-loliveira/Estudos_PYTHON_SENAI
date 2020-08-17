18620378e82bc631a1abb9ea3d25133d54de98fd
peter-loliveira <peter.loliveira@gmail.com>
2020-08-17 16:34:50 -0300

    Refatorando o código com melhores praticas

--- exercicios_Aulas/exercicio02_listas.py
+++ exercicios_Aulas/exercicio02_listas.py
@@ -19,6 +19,7 @@ Obs.: Desenvolver o código-fonte preferencialmente sem utilizar estruturas cond
 
 # Listas
 destinos = ['Miami', 'Barcelona', 'Florença', 'Amsterdã','Tóquio']
+europa = [destinos[1], destinos[2],destinos[3]]
 valores = [5000, 6500, 7500, 8500,10000]
 
 # Captura dados do usuário
@@ -28,14 +29,17 @@ print('Destino 2 -',destinos[1],"${:,.2f}".format(valores[1]))
 print('Destino 3 -',destinos[2],"${:,.2f}".format(valores[2]))
 print('Destino 4 -',destinos[3],"${:,.2f}".format(valores[3]))
 print('Destino 5 -',destinos[4],"${:,.2f}".format(valores[4]),'\n')
+
 destino = int(input('Favor selecione o destino desejado pelo nr: ')) -1
 total_passageiros = int(input('Informe o total de passageiros: '))
 total_viagem = valores[destino] * total_passageiros
 
 # Solcitação d
-print('Seu destino foi:',destinos[destino])
-print('Total de passageiros:', total_passageiros)
-print('O valor será de:',"${:,.2f}".format(total_viagem),'\n' )
+print('O destino selecionado foi {dest} para um total de {passag} {Texto_passageiros_ajustado}. O valor total será de {valor_form}\n'
+.format(dest = destinos[destino], 
+        passag=total_passageiros, 
+        Texto_passageiros_ajustado = 'passageiro' if total_passageiros == 1 else 'passageiros',
+        valor_form="${:,.2f}".format(total_viagem)))
 
 print('O valor total ficou entre o intervalo de $12.000 e $17.000?')
 print('Verdadeiro\n' if total_viagem >= 12000 and total_viagem <= 17000 else 'Falso\n')
@@ -44,9 +48,19 @@ print('O valor pode ser dividido em 3x iguais?')
 print('Verdadeiro\n' if total_viagem %3 == 0 else 'Falso\n')
 
 print('O destino fica FORA da Europa?')
-# quebrei a linha só pra não ficar muito longa
-print('Falso' 
-if destino == destinos.index('Barcelona') 
-or destino == destinos.index('Florença') 
-or destino == destinos.index('Amsterdã') 
-else 'Verdadeiro')
+
+print('1ª forma')
+print('Falso' if destino == destinos.index('Barcelona') or destino == destinos.index('Florença') or destino == destinos.index('Amsterdã') else 'Verdadeiro')
+print('2ª forma')
+
+print('FALSO' if destinos[destino] in europa else 'VERDADEIRO')
+
+
+#FORMA MELHOR
+destino_dic = {
+    'Miami': 5000,
+    'Barcelona': 6500,
+    'Florença': 7500,
+    'Amsterdã': 8500,
+    'Tóquio': 10000
+}

2e32966d33d87ac767797e6a1a77cecc688fc984
peter-loliveira <peter.loliveira@gmail.com>
2020-08-19 14:35:19 -0300

    Condicionais e Laços de repetição

---
+++ cursos_extras/digital_innovation/execrcicios_repeticao.py
@@ -0,0 +1,28 @@
+separador = '\n######################\nLAÇO DE REPETIÇÃO  FOR\n######################\n'
+print(separador)
+
+intervalo = int(input('Entre com um número: '))
+if intervalo <= 0:
+    print('Digite numeros maiores que 0')
+elif intervalo > 2000: 
+    negacao = '\n################################################\nNEM FERRANDO QUE VOU FAZER ESSES CÁLCULOS TODOS!\n################################################\n'
+    print(negacao)
+else:
+    primos = []
+    for x in range(1, intervalo + 1):
+        aux = 0
+        for y in range(1, x+1):
+            if x % y == 0:
+                aux += 1
+        if aux == 2:
+            primos.append(y)
+
+    for indice in range(len(primos)):
+        print('{}º número primo: {}'.format(indice + 1, primos[indice]))
+
+separador = '\n########################\nLAÇO DE REPETIÇÃO  WHILE\n########################\n'
+print(separador)
+
+x = 'S'
+while x == 'S' or x == 's':
+    x = input( 'Deseja continuar (S/N)? ')
